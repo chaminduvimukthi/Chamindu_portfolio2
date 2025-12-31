66bda49d086cdc0a17d49668e33b57ef827622b6
Chamindu Vimukthi <chaminduvimukthi7@gmail.com>
2025-12-31 21:46:13 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("toggleTheme");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+        toggleBtn.textContent = "🌙 Dark Mode";
+    }
+});
